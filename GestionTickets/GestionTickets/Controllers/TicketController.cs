f60a8b3fe000113cd853aa861105c7a19c3f7076
Ninisbeth Segovia Noguera <ninisbeth_segovia@hotmail.com>
2019-08-14 16:10:26 -0500

    Vista para crear nuevo ticket con clases de font awesome

---
+++ GestionTickets/GestionTickets/Controllers/TicketController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestionTickets.Controllers
+{
+    public class TicketController : Controller
+    {
+        // GET: Ticket
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult Nuevo(int id)
+        //{
+        //    RedirectToAction("Index");
+        //}
+    }
+}
